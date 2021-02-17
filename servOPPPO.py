0f2cd50483f9256b946a4020bd18d5a838e8e912
Andrey Grigorevskiy <ag199930@mail.ru>
2021-02-17 17:36:14 +0700

    Первая попытка

---
+++ servOPPPO.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import socket #библиотека сокета
+import string #библиотека строк
+
+print("сервер запущен")
+
+sock = socket.socket() #создаем сокет
+sock.bind(('',9090)) #инициализируем порт
+sock.listen(1) #слушаем 1 это максимальное количество прослушиваний
+conn, addr = sock.accept() #принримает подключение и выозвращяет новый сокет и адрес клиента
+
+print('connected: ', addr)
+
+
+#устанавливаем связь с клиентом и общяемся с ним
+while True:
+    data = conn.recv(1024)
+    if not data:
+        break
+    print(bytes.decode(data, encoding='utf-8'))
+    conn.send(data.upper())
+
+conn.close()
+
+
+
+
+print("сервер остановлен")
