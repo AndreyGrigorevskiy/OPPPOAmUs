4242b49f899881f2160fe6c774aaf88f79106321
Andrey Grigorevskiy <ag199930@mail.ru>
2021-02-19 23:09:15 +0700

    фикс клиента
    пофиксил падение клиента и сервера при отправке пустого сообщения

--- ClientOPPPO.py
+++ ClientOPPPO.py
@@ -8,14 +8,16 @@ import string
 sock = socket.socket()
 sock.connect(('localhost', 9090))
 action = True
+print("to exit write to chat: /exit")
 while action:
-    sock.send(input().encode('utf-8'))
+    message = input()
+    if not message:
+        message = " you are gay"
+    if message == "/exit":
+        break;
+    message = message.encode('utf-8')
+    sock.send(message)
     data = sock.recv(1024)
     print(data)
-    print('continue typing ?(Y\N)')
-    x = input()
-    if x == 'N' or x == 'n':
-        break
-
 
 sock.close()
